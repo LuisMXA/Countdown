d96806d01372e2968609439d832bdef61a514d9a
Luis Aranda <luis.mx.aranda@gmail.com>
2023-07-02 17:30:26 -0300

    initial commit

---
+++ JavaScript/countdown.js
@@ -0,0 +1,39 @@
+const getRemainTime = deadline => {
+     let now = new Date()
+     let remainTime = (new Date(deadline) - now + 1000) / 1000
+     let remainSeconds = ('0' + Math.floor(remainTime % 60)).slice(-2)
+     let remainMinutes = ('0' + Math.floor(remainTime / 60 % 60)).slice(-2)
+     let remainHours = ('0' + Math.floor(remainTime / 3600 % 24)).slice(-2)
+     let remainDays = Math.floor(remainTime / (3600 * 24))
+
+     return {
+          remainTime,
+          remainSeconds,
+          remainMinutes,
+          remainHours,
+          remainDays
+     }
+}
+
+const countDown = (deadline, element, finalMessage) => {
+     const elem = document.getElementById(element)
+
+     const timerUpdate = setInterval(() => {
+          let time = getRemainTime(deadline)
+          const clock = `
+          <div class="counter"><span class="time">${time.remainDays}</span><span class="text">Días</span></div>
+          <div class="counter"><span class="time"> ${time.remainHours}</span><span class="text">Horas</span></div>
+          <div class="counter"><span class="time">${time.remainMinutes}</span><span class="text">Minutos</span></div>
+          <div class="counter"><span class="time">${time.remainSeconds}</span><span class="text">Segundos</span></div>
+          `
+          
+          elem.innerHTML = clock
+
+          if (time.remainTime <= 1) {
+               clearInterval(timerUpdate)
+               elem.innerHTML = finalMessage
+          }
+     }, 1000)
+}
+
+countDown('Sun Oct 21 2023 00:00:00 GMT-0300','countdown','sas')
---
+++ JavaScript/matrix.js
@@ -0,0 +1,49 @@
+const canvas = document.getElementById('canvas')
+const container = document.querySelector('.container')
+const ctx = canvas.getContext('2d')
+
+window.addEventListener('resize', resizeCanvas, false)
+
+const img = document.createElement('img')
+img.src = '../Misc/morpheus.png'
+container.appendChild(img)
+img.setAttribute('class', 'morpheus')
+
+let w = canvas.width
+let h = canvas.height
+let colums = Math.floor(w / 20) + 1
+let axisY = Array(colums).fill(0)
+
+function resizeCanvas() {
+     canvas.width = document.body.offsetWidth
+     canvas.height = document.body.offsetHeight
+     w = canvas.width
+     h = canvas.height
+     colums = Math.floor(w / 20) + 1
+     axisY = Array(colums).fill(0)
+     
+     matrix()
+}
+resizeCanvas()
+
+function matrix () {
+     
+     ctx.fillStyle = '#0002'
+     ctx.fillRect(0, 0, w, h)
+     ctx.fillStyle = '#0f0'
+     ctx.font = '16pt monospace'
+     
+     axisY.forEach((y, index)=> {
+
+          const text = String.fromCharCode(Math.random() * (parseInt('30a0',16) - parseInt('30ff',16)) + (parseInt('30a0',16)))
+          const x = index * 20
+          ctx.fillText(text, x, y)
+          if (y > 100 + Math.random() * 10000) {
+               axisY[index] = 0
+          }else {
+               axisY[index] = y + 20
+          }
+     })
+}
+
+setInterval(matrix, 50)
